fa0ecec3a783cdc90f55392ae346fac2bb6ecb44
Chris Houston <chrisbcoding247@gmail.com>
2021-07-30 10:25:38 -0400

    index file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//Post   /users/register
+router.get('/register', function(req, res, next) {
+  // check for email and password
+  // respond with error if not included
+  // store user details
+
+
 });
 
 module.exports = router;
