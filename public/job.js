43029caebd4a0308d504acff03189388d5e6d942
Suvd Tod <t.suvdaa0424@gmail.com>
2021-08-02 10:24:57 -0500

    added some functions on job.ejs

---
+++ public/job.js
@@ -0,0 +1,23 @@
+
+
+
+
+document.addEventListener('click', (e) => {
+    const jobColumn = e.target.closest('.job-column')
+    if (jobColumn) {
+        const jobDetailsItems = document.getElementsByClassName("job-details")
+        Array.prototype.forEach.call(jobDetailsItems,details => {
+            details.classList.add("d-none")
+            
+        });
+        let jobID = jobColumn.dataset.id
+        const jobDetails = document.getElementById(`job-${jobID}`)
+        jobDetails.classList.remove("d-none")
+    }
+})
+
+// document.addEventListener("click", (e) => {
+//     if (e.target.classList.contains("save")) {
+//         saveToFavorites.target.dataset.jobID
+//     }
+//     })
