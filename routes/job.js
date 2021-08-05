e958331cdc3d423d11e16fa8ca9b8c4cd68bfc66
Chris Houston <chrisbcoding247@gmail.com>
2021-08-04 23:57:09 -0400

    working version

--- routes/job.js
+++ routes/job.js
@@ -3,16 +3,16 @@ var router = express.Router();
 const db = require("../models");
 const { Op } = require("sequelize");
 
-/* GET home page. */
+
 router.get("/search", async (req, res, next) => {
   const locations = await db.Location.findAll();
   const query = {
     title: {
       [Op.iLike]: "%" + req.query.search + "%",
     }
-  } 
+  }
   // if location exists 
-  if (req.query.location){
+  if (req.query.location) {
     // add to query
     query.LocationId = req.query.location
   }
@@ -30,33 +30,9 @@ router.get("/search", async (req, res, next) => {
   });
 });
 
-// router.get('/search', (req, res, next) => {
-//   const title = db.Job.findAll({
-//     where: {
-//       title: {
-//         [Sequelize.Op.iLike]: title
-//       }
-//     }
-//   });
-//   console.log(title)
-// })
 
-// router.post('/', (req, res) => {
 
-//   document.addEventListener('click', (e) => {
-//     if (e.target.classList.contains('job-column')) {
-//         let jobID = e.target.dataset.id
-//         db.Job.findOne({
-//             where: {
-//                 id: req.body.id
-//             }
-//         })
-//             .then((job) => {
 
-//             })
-//     }
-// })
 
-// })
 
 module.exports = router;
