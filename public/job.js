7c2355daf4ca5abe65d8b4ea7c4d5635921a2725
Chris Houston <chrisbcoding247@gmail.com>
2021-08-23 12:29:46 -0400

    delete fav route complete

--- public/job.js
+++ public/job.js
@@ -1,9 +1,11 @@
+// Connected to Jobs.ejs
 
 
-
-
+// Toggles display for hidden jobs info upon click
 document.addEventListener('click', (e) => {
+    
     const jobColumn = e.target.closest('.job-column')
+
     if (jobColumn) {
         const jobDetailsItems = document.getElementsByClassName("job-details")
         Array.prototype.forEach.call(jobDetailsItems,details => {
@@ -16,25 +18,27 @@ document.addEventListener('click', (e) => {
     }
 })
 
-
-    document.addEventListener('click', event => {
-        const favButton = event.target.closest(".save")
-        if (favButton){
-
-            event.preventDefault()
-            
-
-            
-            const id = favButton.dataset.id
-            fetch(`/users/favorites/${id}/add`, {
-                method: "POST"
+// Saves jobs to favorites after clicking save button
+document.addEventListener('click', event => {
+    // Grab fav button 
+    const favButton = event.target.closest(".save")
+    // if favButton is clicked
+    if (favButton){
+        // prevent default
+        event.preventDefault()
+        // save the id of the selected favorite
+        const id = favButton.dataset.id
+        // Post that job ID to favorites using post route
+        fetch(`/users/favorites/${id}/add`, {
+            method: "POST"
+        })
+        // Then remove the favorite button from being visible
+            .then(res => res.json())
+            .then(data => {
+                favButton.remove()
             })
-                .then(res => res.json())
-                .then(data => {
-                    favButton.remove()
-                })
-        }
+    }
 
-    })
+})
 
 
